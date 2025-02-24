c27bba5c6c583fa9b617de045429728f4b41af07
Josh Mutus <joshmutus@Mac.lan>
2025-02-23 21:39:56 -0800

    Get sprite sheet working.

--- open_window.py
+++ open_window.py
@@ -8,8 +8,26 @@ import arcade
 # Constants
 WINDOW_WIDTH = 320
 WINDOW_HEIGHT = 240
-WINDOW_TITLE = "Platformer"
+WINDOW_TITLE = "Strong Town"
 
+
+class Player(arcade.Sprite):
+    def __init__(self, texture_list: list[arcade.Texture]):
+        super().__init__(texture_list[0])
+        self.textures = texture_list
+        self.time_elapsed = 0
+
+    def update(self, delta_time = 1 / 60, *args, **kwargs) -> None:
+        self.time_elapsed += delta_time 
+
+        if self.time_elapsed > 0.1:
+            if self.cur_texture_index < len(self.textures):
+                self.set_texture(self.cur_texture_index)  
+                self.cur_texture_index += 1  
+            self.time_elapsed = 0
+
+        if self.cur_texture_index == 23:
+            self.cur_texture_index = 0
 
 class GameView(arcade.Window):
     """
@@ -20,8 +38,14 @@ class GameView(arcade.Window):
 
         # Call the parent class to set up the window
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
-
+        self.sprite_list = arcade.SpriteList()
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+        char_sheet  = arcade.load_spritesheet("assets/player/player_actions.png")
+        self.texture_list = char_sheet.get_texture_grid(size=(48,48), columns=2, count=24)
+        print("TEXTURE LEN: ", len(self.texture_list))
+        self.player = Player(self.texture_list)
+        self.player.position = 200,200
+        self.sprite_list.append(self.player)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
@@ -35,8 +59,12 @@ class GameView(arcade.Window):
         # set to. This ensures that you have a clean slate for drawing each
         # frame of the game.
         self.clear()
-
+        self.sprite_list.draw()
+        
         # Code to draw other things will go here
+
+    def on_update(self, delta_time: float) -> None:
+        self.sprite_list.update() 
 
 
 def main():
