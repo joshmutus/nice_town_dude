ccee6e72e116dd51e5fb68518409cf68d1cf5344
Josh Mutus <joshmutus@Joshs-MacBook-Pro.local>
2025-03-15 09:01:54 -0700

    Reorg test

--- tests/test_custom_sprites.py
+++ tests/test_custom_sprites.py
@@ -1,4 +1,4 @@
-import nice_town_dude.graphics_helpers as gh
+import nice_town_dude.custom_sprites as cs
 import numpy as np
 from numpy.testing import assert_array_equal
 
@@ -8,13 +8,15 @@ def test_square():
     test_array_border_3_1 = np.array([[x,x,x],
                                       [x,o,x],
                                       [x,x,x]])
-    assert_array_equal(test_array_border_3_1, gh.create_square_border_array(size=3,border_width=1))
+    assert_array_equal(test_array_border_3_1, cs.create_square_border_array(size=3,border_width=1))
     test_array_border_6_2 = np.array([[x,x,x,x,x,x],
                                       [x,x,x,x,x,x],
                                       [x,x,o,o,x,x],
                                       [x,x,o,o,x,x],
                                       [x,x,x,x,x,x],
                                       [x,x,x,x,x,x],])
-    assert_array_equal(test_array_border_6_2, gh.create_square_border_array(size=6,border_width=2))
+    assert_array_equal(test_array_border_6_2, cs.create_square_border_array(size=6,border_width=2))
+
+def test_
 
     
